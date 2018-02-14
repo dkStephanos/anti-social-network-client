4ff65883902b15a0a3138e2df97d6f92016d00f8
Dembe Koi <stephanos@goldmail.etsu.edu>
2018-02-14 13:45:56 -0500

    Added Login Page/Login Button/GitHub Icon

---
+++ src/components/LoginButton.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Button from 'react-toolbox/lib/button/Button';
+import GithubIcon from './GithubIcon';
+import './LoginButton.css';
+
+const authorizeUrl = 'https://github.com/login/oauth/authorize';
+const clientId = '7f7d6103e2acb6a28cb7';
+const scope = 'user';
+
+class LoginButton extends Component {
+  render() {
+    return (
+      <Button
+        raised
+        accent
+        href={`${authorizeUrl}?client_id=${clientId}&scope=${scope}`}
+      >
+        <GithubIcon /> Login with GitHub
+      </Button>
+    );
+  }
+}
+
+export default LoginButton;
